edd616b74e783a285fec79bb96de064c34465d81
Abhishek Sah <abhi.sah.97@gmail.com>
2019-02-24 15:22:51 +0530

    Adds Canteen Contract

---
+++ CanteenContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+
+contract CanteenContract {
+  uint256 canteenBalance;
+  uint256 personBalance;
+  uint256 governmentBalance;
+
+  mapping (bytes32 => uint) public menu;
+
+  function CanteenContract(){
+    canteenBalance = 0;
+    personBalance = 0;
+    governmentBalance = 0;
+  }
+
+}
