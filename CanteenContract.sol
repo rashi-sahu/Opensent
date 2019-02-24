da411568cda6cdded3471a1c67ac6fc9c84f3b6f
Abhishek Sah <abhi.sah.97@gmail.com>
2019-02-24 16:25:08 +0530

    Add getter methods to fetch balances of different stakeholders

--- CanteenContract.sol
+++ CanteenContract.sol
@@ -26,6 +26,16 @@ contract CanteenContract {
     personBalance -= totalPrice;
   }
 
+  function getGovernmentBalance() returns (uint256){
+    return governmentBalance;
+  }
+  function getCanteenBalance() returns (uint256){
+    return canteenBalance;
+  }
+  function getPersonBalance() returns (uint256){
+    return personBalance;
+  }
+
   function validItem(bytes32 item) returns (bool){
     if(item == "Maggi" || item == "Burger" || item == "Paratha" || item == "Pasta")
       return true;
