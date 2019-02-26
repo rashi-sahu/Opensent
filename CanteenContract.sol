19e7db2de3597498f134876c81e0ac024bbc89ad
Abhishek Sah <abhi.sah.97@gmail.com>
2019-02-26 11:02:33 +0530

    Fixes indentation in contract

--- CanteenContract.sol
+++ CanteenContract.sol
@@ -1,45 +1,45 @@
 pragma solidity ^0.4.11;
 
 contract CanteenContract {
-  uint256 canteenBalance;
-  uint256 personBalance;
-  uint256 governmentBalance;
+    uint256 canteenBalance;
+    uint256 personBalance;
+    uint256 governmentBalance;
 
-  mapping (bytes32 => uint256) public menu;
+    mapping (bytes32 => uint256) public menu;
 
-  function CanteenContract(){
-    canteenBalance = 0;
-    personBalance = 0;
-    governmentBalance = 0;
-    menu["Maggi"] = 20;
-    menu["Burger"] = 40;
-    menu["Paratha"] = 30;
-    menu["Pasta"] = 80;
-  }
+    function CanteenContract() public{
+        canteenBalance = 0;
+        personBalance = 0;
+        governmentBalance = 0;
+        menu["Maggi"] = 20;
+        menu["Burger"] = 40;
+        menu["Paratha"] = 30;
+        menu["Pasta"] = 80;
+    }
 
-  function buyItem(bytes32 item) {
-    if(validItem(item)==false) throw;
-    uint256 price = menu[item];
-    uint256 totalPrice = price+5;
-    canteenBalance += price;
-    governmentBalance += 5;
-    personBalance -= totalPrice;
-  }
+    function buyItem(bytes32 item) public{
+        // if(validItem(item)==false) throw;
+        uint256 price = menu[item];
+        uint256 totalPrice = price+5;
+        canteenBalance += price;
+        governmentBalance += 5;
+        personBalance -= totalPrice;
+    }
 
-  function getGovernmentBalance() returns (uint256){
-    return governmentBalance;
-  }
-  function getCanteenBalance() returns (uint256){
-    return canteenBalance;
-  }
-  function getPersonBalance() returns (uint256){
-    return personBalance;
-  }
+    function getGovernmentBalance() public returns (uint256){
+        return governmentBalance;
+    }
+    function getCanteenBalance() public returns (uint256){
+        return canteenBalance;
+    }
+    function getPersonBalance() public returns (uint256){
+        return personBalance;
+    }
 
-  function validItem(bytes32 item) returns (bool){
-    if(item == "Maggi" || item == "Burger" || item == "Paratha" || item == "Pasta")
-      return true;
-    return false;
-  }
+    function validItem(bytes32 item) public returns (bool){
+        if(item == "Maggi" || item == "Burger" || item == "Paratha" || item == "Pasta")
+          return true;
+        return false;
+    }
 
 }
