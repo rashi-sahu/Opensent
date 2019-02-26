1a4742830e05b0c0954150d80c61f1c1e150e274
rashi-sahu <rashi.sahu13@gmail.com>
2019-02-26 18:54:29 +0530

    Client fetches balances of all stakeholders

--- public/index.js
+++ public/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {  
+  fetch('/balances')
+  .then(res => res.json())
+  .then(res => {
+    const balancesTableHTML = res.candidates.map(function(candidate) {
+      return `<tr><td>${stakeholder.name}</td><td id='${stakeholder.name}'>${stakeholder.balance}</td></tr>`;
+    });
+
+    $('#balancesTable').html(balancesTableHTML);
+  }).catch(function(err) {
+    // Error :(
+  });
+
   $('#buyItem').click(function(event) {
     const headers = new Headers({
       "Content-Type": "application/json",
