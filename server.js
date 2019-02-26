5a8f75fd828dc1681efc8ac3e9e56ff518b2bceb
rashi-sahu <rashi.sahu13@gmail.com>
2019-02-26 19:17:09 +0530

    Adds controller to fetch balaces of all stakeholders

--- server.js
+++ server.js
@@ -26,6 +26,22 @@ app.post('/buy', function (req, res) {
   }
 });
 
+app.get('/balances', function(req, res) {
+  var stakeholders = ['Person', 'Canteen', 'Government'];
+    try {
+      const stakeholderBalances = stakeholders.map(function(stakeholders) {
+        const personBalance = contractInstance.getPersonBalance.call({ from: web3.eth.accounts[0] }).toString();
+        const canteenBalance = contractInstance.getCanteenBalance.call({ from: web3.eth.accounts[0] }).toString();
+        const governmentBalance = contractInstance.getGovernmentBalance.call({ from: web3.eth.accounts[0] }).toString();
+        balances = {personBalance, canteenBalance, governmentBalance};
+        console.log(balances);
+      });
+      res.send({ balances: balances });
+    } catch (e) {
+      res.status('400').send(`Failed! ${e}`);
+    }
+});
+
 app.listen(3000, function () {
   console.log('App ready and listening on port 3000!')
 });
